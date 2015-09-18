cc12f495eff7a49a6480b7e96b8f159bc09688f2
Vagner Oliveira <vagner@vnda.com.br>
2015-09-18 15:45:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var data_size = parseInt($(".view-also .relacionados").data('size'));
+
+var uniqueRandoms = [];
+var numRandoms = data_size;
+
+function makeUniqueRandom() {
+  // refill the array if needed
+  if (!uniqueRandoms.length) {
+      for (var i = 0; i < numRandoms; i++) {
+          uniqueRandoms.push(i);
+      }
+  }
+
+  var index = Math.floor(Math.random() * uniqueRandoms.length);
+  var val = uniqueRandoms[index];
+
+  // now remove that value from the array
+  uniqueRandoms.splice(index, 1);
+
+  return val;
+
+}
+
+for (var i = 0; i < 3; i++) {
+    var rand = makeUniqueRandom();
+    if (i % numRandoms == 0) {
+        // $("#results").append("----<br>");
+        console.log("numRandoms"+rand)
+    }
+    console.log(rand)
+    $("li.item_"+rand+"").removeClass("hide")
+}
